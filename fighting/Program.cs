d31c04b59ff69f755a4441efdce18cd32fdd33ee
Bilal Asry <bilal.asry@elev.ga.ntig.se>
2024-09-19 10:34:20 +0200

    fighting

--- fighting/Program.cs
+++ fighting/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿while (true)
+{int dmg =Random.Shared.Next(50, 100);
+Console.WriteLine(dmg);
+Console.ReadLine();
+}
+
+
+Console.WriteLine("Skriv ditt namn HÄR--->:");
+string name = Console.ReadLine();
+
+while (name == "")
+{
+    Console.WriteLine("Hmar skärp dig dachri lek inte speciel");
+    Console.WriteLine("Försök igen hmar");
+    name = Console.ReadLine();
+}
+Console.ReadLine();
